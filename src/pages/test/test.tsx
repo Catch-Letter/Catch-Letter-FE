758150d0c1d22a77ce54108e15852b087aa082a6
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-01-17 16:01:01 +0900

    feat: test 페이지에서 컴포넌트 스샷을 위한 util css 추가

--- src/pages/test/test.tsx
+++ src/pages/test/test.tsx
@@ -1,9 +1,12 @@
+import { testContainerStyles } from '#/shared/utils/styles/test-container'
 import { css } from '@emotion/react'
 
 const Test = () => {
   return (
     <div
       css={css`
+        /* ${testContainerStyles} */
+        color: white;
         height: 100vh;
         background-color: #444;
       `}
