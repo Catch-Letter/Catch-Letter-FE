5e6b1e880d6f4bacc40b9226eee5dd23998b1ffd
jizero <jizero27@gmail.com>
2025-01-20 01:11:47 +0900

    fix: 버튼 호버 css 수정 (#8)

--- src/shared/ui/button/Button.styles.tsx
+++ src/shared/ui/button/Button.styles.tsx
@@ -3,25 +3,21 @@ import { colors } from '#/styles/color'
 import { ButtonProps } from '#/shared/ui/button/Button'
 
 export const buttonStyle = ({ variant, width }: ButtonProps) => css`
+  display: flex;
   padding: 12px 24px;
   justify-content: center;
   align-items: center;
   border: none;
   border-radius: 8px;
   cursor: pointer;
-  width: ${width !== undefined && `${width}px`};
+  width: ${width && `${width}px`};
   background-color: ${variant === 'primary' ? colors.neonGreen[5] : colors.grey[8]};
   color: ${variant === 'primary' ? colors.grey[13] : colors.grey[1]};
   font-size: 18px;
   font-weight: 700;
 
-  &:hover {
+  &:hover:not(:disabled) {
     background-color: ${variant === 'primary' ? colors.neonGreen[8] : colors.grey[9]};
-  }
-
-  &:hover:disabled {
-    background-color: ${variant === 'primary' ? colors.neonGreen[5] : colors.grey[8]};
-    cursor: not-allowed;
   }
 
   &:disabled {
