a252de0a5c370aa930b738d876221f9430c6139d
jizero <jizero27@gmail.com>
2025-01-20 20:17:53 +0900

    feat: SelectPattern 컴포넌트 생성 (#15)

--- src/pages/letter-choice/ui/select-pattern/SelectPattern.tsx
+++ src/pages/letter-choice/ui/select-pattern/SelectPattern.tsx
@@ -1,21 +1,28 @@
-import { useState } from 'react'
 import { PatternStyle, SelectPatternStyle } from './SelectPattern.styles'
+import { usePatternStore } from '#/store/colorStore'
 
-const patterns = ['/redline.svg', '/Mask1.svg', '/Mask2.svg', '/Mask3.svg']
+const patterns = {
+  default: '/redline.svg',
+  line: '/Mask1.svg',
+  dot: '/Mask2.svg',
+  grid: '/Mask3.svg',
+} as const
 
 const SelectPattern = () => {
-  const [selectedPattern, setSelectedPattern] = useState('')
+  const selectedPattern = usePatternStore((state) => state.selectedPattern)
+  const setSelectedPattern = usePatternStore((state) => state.setSelectedPattern)
+
   return (
     <div css={SelectPatternStyle}>
       <ul>
-        {patterns.map((pattern, idx) => (
+        {Object.keys(patterns).map((pattern, idx) => (
           <li
             css={PatternStyle}
             key={idx}
             className={selectedPattern === pattern ? 'active' : ''}
-            onClick={() => setSelectedPattern(pattern)}
+            onClick={() => setSelectedPattern(pattern as keyof typeof patterns)}
           >
-            <img src={pattern} alt='패턴' />
+            <img src={patterns[pattern as keyof typeof patterns]} alt='패턴' />
           </li>
         ))}
       </ul>
--- src/store/colorStore.ts
+++ src/store/colorStore.ts
@@ -1,13 +1,23 @@
 import { create } from 'zustand'
 
-type ColorType = 'grey' | 'pink' | 'violet' | 'green' | 'blue'
+export type ColorType = 'grey' | 'pink' | 'violet' | 'green' | 'blue'
+export type PatternType = 'default' | 'dot' | 'line' | 'grid'
 
 interface ColorStore {
   selectedColor: ColorType
   setSelectedColor: (color: ColorType) => void
 }
 
+interface PatternStore {
+  selectedPattern: PatternType
+  setSelectedPattern: (pattern: PatternType) => void
+}
 export const useColorStore = create<ColorStore>((set) => ({
   selectedColor: 'grey',
   setSelectedColor: (color) => set({ selectedColor: color }),
+}))
+
+export const usePatternStore = create<PatternStore>((set) => ({
+  selectedPattern: 'default',
+  setSelectedPattern: (pattern) => set({ selectedPattern: pattern }),
 }))
