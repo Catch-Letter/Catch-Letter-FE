c0c0600aa87d82bd2abfd1508b174d0e792f22dc
jizero <jizero27@gmail.com>
2025-01-21 16:24:31 +0900

    feat: 편지 고르기 페이지 퍼블리싱 (#15)

---
+++ src/pages/letter-choice/ChoiceLetter.styles.tsx
@@ -0,0 +1,13 @@
+import { colors } from '#/styles/color'
+import { css } from '@emotion/react'
+
+export const ChoiceLetterStyle = css`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  background-color: ${colors.grey[13]};
+
+  .content {
+    padding: 16px 63px 34px;
+  }
+`
