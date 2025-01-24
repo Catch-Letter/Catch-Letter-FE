3037b3a12b23e446854adb4978eb065221f4c53e
jizero <jizero27@gmail.com>
2025-01-24 16:26:24 +0900

    design: 편지 전송 완료 페이지 퍼블리싱 (#15)

---
+++ src/pages/letter-send/SendLetter.styles.tsx
@@ -0,0 +1,76 @@
+import { colors } from '#/styles/color'
+import { css, keyframes } from '@emotion/react'
+
+const cardMove = keyframes`
+  0% {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(90px); 
+    opacity: 0.8; 
+  }
+`
+
+export const SendLetterStyle = css`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: white;
+  background-color: ${colors.violet[5]};
+  text-align: center;
+
+  .card-area {
+    position: relative;
+    width: 320px;
+    height: 224px;
+    overflow: hidden;
+  }
+
+  .card-container {
+    position: absolute;
+    top: 0;
+    left: 25%;
+    transform: translateX(-50%);
+    animation: ${cardMove} 1s forwards;
+  }
+
+  svg {
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+  }
+
+  .svg-1 {
+    bottom: 0;
+    z-index: 1;
+
+    g {
+      mix-blend-mode: plus-lighter;
+    }
+  }
+
+  .svg-2 {
+    bottom: -10px;
+    z-index: 2;
+
+    g {
+      mix-blend-mode: plus-lighter;
+      opacity: 0.6;
+    }
+  }
+
+  .bar {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 247px;
+    height: 4px;
+    background-color: white;
+    z-index: 3;
+  }
+`
---
+++ src/pages/letter-send/ui/desc-send/index.ts
@@ -0,0 +1,1 @@
+export { default as SendDesc } from './SendDesc'
