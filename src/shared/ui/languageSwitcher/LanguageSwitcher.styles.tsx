4731f4ec4821ca592f52c7fc9049141259fa9cc3
Sonseongoh <nobleser@naver.com>
2025-01-29 01:46:33 +0900

    feat: 코드리뷰 반영 (#19)

--- src/shared/ui/languageSwitcher/LanguageSwitcher.styles.tsx
+++ src/shared/ui/languageSwitcher/LanguageSwitcher.styles.tsx
@@ -1,12 +1,12 @@
-// import { colors } from '#/styles/color'
 import { css } from '@emotion/react'
+import { colors } from '#/styles/color'
 
 export const buttonStyle = (isKorean: boolean) => css`
   display: flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(60, 62, 65, 0.5);
-  color: white;
+  background-color: ${colors.grey[7]};
+  color: ${colors.grey[1]};
   border: 1px solid #98999b;
   padding: 4px 8px;
   font-size: 14px;
@@ -17,14 +17,14 @@ export const buttonStyle = (isKorean: boolean) => css`
   span {
     padding: 0 5px;
     font-weight: bold;
-    color: ${isKorean ? 'white' : '#d3d3d3'};
+    color: ${isKorean ? colors.grey[1] : colors.grey[5]};
   }
 
   span + span {
-    color: ${isKorean ? '#d3d3d3' : 'white'};
+    color: ${isKorean ? colors.grey[5] : colors.grey[1]};
   }
 
   &:hover {
-    background-color: gray;
+    background-color: ${colors.grey[8]};
   }
 `
--- src/shared/ui/languageSwitcher/languageSwitcher.tsx
+++ src/shared/ui/languageSwitcher/languageSwitcher.tsx
@@ -1,13 +1,12 @@
-import React from 'react'
 import { useTranslation } from 'react-i18next'
 import { buttonStyle } from './LanguageSwitcher.styles'
 
-const LanguageSwitcher: React.FC = () => {
+const LanguageSwitcher = () => {
   const { i18n } = useTranslation()
   const isKorean = i18n.language === 'ko_KR'
 
   const toggleLanguage = () => {
-    const newLanguage = i18n.language === 'en_US' ? 'ko_KR' : 'en_US'
+    const newLanguage = isKorean ? 'en_US' : 'ko_KR'
     i18n.changeLanguage(newLanguage)
   }
 
