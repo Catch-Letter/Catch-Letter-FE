eda64f171da8d5df3a85fd797aabfef2ef5a8569
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-01-31 00:25:31 +0900

    resolve merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Layout } from '#/app/layout'
 import { Home, Test } from '#/pages'
 import { BrowserRouter, Route, Routes } from 'react-router'
-
+import { I18nextProvider } from 'react-i18next'
+import i18n from './utils/i18n'
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path='/test' element={<Test />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <I18nextProvider i18n={i18n}>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Layout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/test' element={<Test />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </I18nextProvider>
   )
 }
 
