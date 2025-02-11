59f977ccdf7daad8576f97e9c5bf2c5bb8e09fd8
jizero <jizero27@gmail.com>
2025-02-12 00:15:43 +0900

    feat: 편지 전송 api 연동 (#37)

--- src/pages/letter-choice/ChoiceLetter.tsx
+++ src/pages/letter-choice/ChoiceLetter.tsx
@@ -2,26 +2,59 @@ import { LetterCard } from '#/components'
 import { LetterContent, Tab } from '#/components/letter-choice'
 import { Button } from '#/shared/ui/button'
 import { useLetterCreationStore } from '#/store/letterCreateStore'
+import { useLocation, useNavigate } from 'react-router'
 import { ChoiceLetterStyle } from './ChoiceLetter.styles'
-
-const data = {
-  to: '친구',
-  content: `이 편지는 영국에서 최초로 시작되어 일년에 한바퀴를 돌면서 받는 사람에게 행운을 주었고 지금은 당신에게로 옮겨진 이 편지는 4일 안에 당신 곁을 떠나야 합니다. 이 편지를 포함해서 7통을 행운이 필요한 사람에게 보내 주셔야 합니다. 복사를 해도 좋습니다. 혹 미신이라 하실지 모르지만 사실입니다.
-영국에서 HGXWCH이라는 사람은 1930년에 이 편지를 받았습니다. 그는 비서에게 복사해서 보내라고 했습니다. 며칠 뒤에 복권이 당첨되어 20억을 받았습니다. 어떤 이는 이 편지를 받았으나 96시간 이내 자신의 손에서 떠나야 한다는 사실을 잊었습니다. 그는 곧 사직되었습니다. 나중에야 이 사실을 알고 7통의 편지를 보냈는데 다시 좋은 직장을 얻었습니다. 미국의 케네디 대통령은 이 편지를 받았지만 그냥 버렸습니다. 결국 9일 후 그는 암살당했습니다. 기억해 주세요. 이 편지를 보내면 7년의 행운이 있을 것이고 그렇지 않으면 3년의 불행이 있을 것입니다. 그리고 이 편지를 버리거나 낙서를 해서는 절대로 안됩니다. 7통입니다. 이 편지를 받은 사람은 행운이 깃들 것입니다. 힘들겠지만 좋은 게 좋다고 생각하세요. 7년의 행운을 빌면서...`,
-  from: '친구',
-}
+import { apiClient } from '#/api/apiClient'
+import { API_ENDPOINTS } from '#/api/apiEndpoints'
 
 const ChoiceLetter = () => {
-  const { selectedColor, selectedFont, selectedPattern } = useLetterCreationStore()
+  //TODO:tempId는 추후 params로 받아오는걸로 교체 필요, letterId는 그림작성 페이지에서 상태값 받아와야됨
+  const navigate = useNavigate()
+  const location = useLocation()
+  const letterInfo = location.state
+  const { selectedColor, selectedFont, selectedPattern, resetStore } = useLetterCreationStore()
+  const tempId = '0292b67c-970e-445a-9ba0-2dd47ae7d95a'
+  const letterId = 7
+
+  const handleSendLetter = async (uuid: string, letterId: number) => {
+    try {
+      const res = await apiClient.post(API_ENDPOINTS.SEND_LETTER(uuid, letterId), {
+        to: letterInfo.to,
+        from: letterInfo.from,
+        contents: letterInfo.content,
+        etc: JSON.stringify({
+          color: selectedColor,
+          font: selectedFont,
+          pattern: selectedPattern,
+        }),
+      })
+
+      navigate('/sendsuccess')
+      resetStore()
+      return res.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
+  const handlePrev = () => {
+    navigate('/writeletter', {
+      state: {
+        to: letterInfo.to,
+        content: letterInfo.content,
+        from: letterInfo.from,
+      },
+    })
+  }
 
   return (
     <div css={ChoiceLetterStyle}>
       <div className='content'>
         <LetterCard type={selectedColor}>
           <LetterContent
-            to={data.to}
-            content={data.content}
-            from={data.from}
+            to={letterInfo.to}
+            content={letterInfo.content}
+            from={letterInfo.from}
             color={selectedColor}
             pattern={selectedPattern}
             font={selectedFont}
@@ -30,10 +63,12 @@ const ChoiceLetter = () => {
       </div>
       <Tab />
       <div className='button-area'>
-        <Button variant='secondary' width={82}>
+        <Button variant='secondary' width={82} onClick={handlePrev}>
           이전
         </Button>
-        <Button width={245}>편지 보내기</Button>
+        <Button width={245} onClick={() => handleSendLetter(tempId, letterId)}>
+          편지 보내기
+        </Button>
       </div>
     </div>
   )
--- src/pages/letter-write/LetterWrite.tsx
+++ src/pages/letter-write/LetterWrite.tsx
@@ -2,35 +2,67 @@ import { LetterWriteStyle, letterWriteWrapper } from './LetterWrite.styles'
 import { Input, Button } from '#/shared/ui'
 import { WriteDesc, TextCard } from '#/components/letter-write'
 import { useTranslation } from 'react-i18next'
-import { BackHeader } from '#/components'
+import React, { useState } from 'react'
+import { useLocation, useNavigate } from 'react-router'
 
 const LetterWrite = () => {
   const { t } = useTranslation()
+  const navigate = useNavigate()
+  const location = useLocation()
+  const [recipient, setRecipient] = useState(location.state?.to || '')
+  const [sender, setSender] = useState(location.state?.from || '')
+  const [content, setContent] = useState(location.state?.content || '')
+
+  const handleChoiceLetter = () => {
+    navigate('/choiceletter', {
+      state: {
+        to: recipient,
+        content,
+        from: sender,
+      },
+    })
+  }
   return (
     <div css={LetterWriteStyle}>
-      <BackHeader Center='비밀편지 쓰기' />
-      <div css={letterWriteWrapper}>
-        <div className='content'>
-          <div className='input-to'>
-            <label className='input-label'>TO</label>
-            <Input placeholder={t('write.to')} />
-          </div>
-          <TextCard color='#f4f4f5' placeholder={t('write.content')} />
-          <div className='input-from'>
-            <label className='input-label'>FROM</label>
-            <Input placeholder={t('write.from')} />
-          </div>
+      <div className='content'>
+        <div className='input-to'>
+          <label className='input-label'>TO</label>
+          <Input
+            placeholder={t('write.to')}
+            value={recipient}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setRecipient(e.target.value)
+            }}
+          />
         </div>
-        <WriteDesc
-          title={t('write.help')}
-          descs={t('write.explain', { returnObjects: true }) as string[]}
+        <TextCard
+          color='#f4f4f5'
+          placeholder={t('write.content')}
+          value={content}
+          onChange={(e: React.ChangeEvent<HTMLTextAreaElement>) => setContent(e.target.value)}
         />
-        <div className='button-area'>
-          <Button variant='secondary' width={82}>
-            {t('before')}
-          </Button>
-          <Button>{t('write.theme')}</Button>
+        <div className='input-from'>
+          <label className='input-label'>FROM</label>
+          <Input
+            placeholder={t('write.from')}
+            value={sender}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+              setSender(e.target.value)
+            }}
+          />
         </div>
+      </div>
+      <WriteDesc
+        title={t('write.help')}
+        descs={t('write.explain', { returnObjects: true }) as string[]}
+      />
+      <div className='button-area'>
+        <Button variant='secondary' width={82}>
+          {t('before')}
+        </Button>
+        <Button onClick={handleChoiceLetter} disabled={!recipient || !sender || !content}>
+          {t('write.theme')}
+        </Button>
       </div>
     </div>
   )
