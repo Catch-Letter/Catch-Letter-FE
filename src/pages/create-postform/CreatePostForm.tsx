491a0862f96b10fe1b461d9ef7f61b9aaa69c723
jizero <jizero27@gmail.com>
2025-02-14 00:40:33 +0900

    feat: 우체통 생성 엔드포인트 작성 및 api 연동 (#38)

--- src/pages/create-postform/CreatePostForm.tsx
+++ src/pages/create-postform/CreatePostForm.tsx
@@ -1,17 +1,57 @@
 import { BackHeader } from '#/components'
 import { Button, InputField } from '#/shared/ui'
+import { useState } from 'react'
 import { CreateFormStyle, FormWrapper } from './CreatePostForm.styles'
+import { apiClient } from '#/api/apiClient'
+import { API_ENDPOINTS } from '#/api/apiEndpoints'
+import { useNavigate } from 'react-router'
 
 const CreatePostForm = () => {
+  const navigate = useNavigate()
+  const [name, setName] = useState('')
+  const [password, setPassword] = useState('')
+
+  const handleCreatePost = async () => {
+    try {
+      const res = await apiClient.post(API_ENDPOINTS.CREATE, {
+        name,
+        password,
+      })
+      navigate('/success', {
+        state: {
+          uuid: res.data.data.uuid,
+          expired: res.data.data.expired_at,
+        },
+      })
+      return res.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div css={CreateFormStyle}>
       <BackHeader Center='우체통 발급' />
       <div css={FormWrapper}>
         <div className='form'>
-          <InputField label='우체통 이름' placeholder='우체통 이름을 지어주세요.' />
-          <InputField label='우체통 비밀번호' placeholder='우체통 이름을 지어주세요.' />
+          <InputField
+            label='우체통 이름'
+            placeholder='우체통 이름을 지어주세요.'
+            value={name}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
+          />
+          <InputField
+            label='우체통 비밀번호'
+            placeholder='우체통 이름을 지어주세요.'
+            type='password'
+            value={password}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPassword(e.target.value)}
+          />
+          <span className='notice'>우체통 비밀번호는 우체통을 열때마다 필요해요!</span>
         </div>
-        <Button width={343}>확인</Button>
+        <Button width={343} onClick={handleCreatePost} disabled={!name && !password}>
+          확인
+        </Button>
       </div>
     </div>
   )
--- src/pages/success/Success.tsx
+++ src/pages/success/Success.tsx
@@ -2,19 +2,25 @@ import { SuccessStyle, SuccessWrapper } from './Success.styles'
 import { Button } from '#/shared/ui'
 import { TimeArea, DescLink } from '#/components/success'
 import { BackHeader } from '#/components'
+import { useLocation } from 'react-router'
+import countTimer from '#/shared/utils/countTimer'
 
 const Success = () => {
+  const location = useLocation()
+  const user = location.state
   const title = '링크가 생성되었어요!'
-  const link = 'https://www.naver.com'
+  const link = `http://localhost:3000/${user.uuid}`
   const btnName = '링크복사'
   const desc = `링크는 재전송 되지 않아요 \n 링크 복사 후 기억할 수 있는 곳에 복사해두세요!`
+  const leftTime = countTimer(user.expired)
+  console.log(countTimer(user.expired))
 
   return (
     <div css={SuccessStyle}>
       <BackHeader />
       <div css={SuccessWrapper}>
         <DescLink title={title} link={link} btnName={btnName} desc={desc} />
-        <TimeArea title='우체통 열람가능 시간' time='24:00:00' />
+        <TimeArea title='우체통 열람가능 시간' time={leftTime} />
         <Button width={343}>내 우체통 공유하기!</Button>
       </div>
     </div>
