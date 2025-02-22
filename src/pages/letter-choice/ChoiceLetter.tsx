6369a7c9e1dbbaa784a9d4fae88b6cbef125f70c
jizero <jizero27@gmail.com>
2025-02-22 13:56:17 +0900

    fix: 글쓰기 api 연동 복구 (#37)

--- src/pages/letter-choice/ChoiceLetter.tsx
+++ src/pages/letter-choice/ChoiceLetter.tsx
@@ -4,12 +4,38 @@ import { Button } from '#/shared/ui/button'
 import { useLetterCreationStore } from '#/store/letterCreateStore'
 import { useLocation, useNavigate } from 'react-router'
 import { ChoiceLetterStyle } from './ChoiceLetter.styles'
+import { apiClient } from '#/api/apiClient'
+import { API_ENDPOINTS } from '#/api/apiEndpoints'
 
 const ChoiceLetter = () => {
   const navigate = useNavigate()
   const location = useLocation()
   const letterInfo = location.state
-  const { selectedColor, selectedFont, selectedPattern } = useLetterCreationStore()
+  const { selectedColor, selectedFont, selectedPattern, resetStore } = useLetterCreationStore()
+
+  const tempId = '0292b67c-970e-445a-9ba0-2dd47ae7d95a'
+  const letterId = 7
+
+  const handleSendLetter = async (uuid: string, letterId: number) => {
+    try {
+      const res = await apiClient.post(API_ENDPOINTS.SEND_LETTER(uuid, letterId), {
+        to: letterInfo.to,
+        from: letterInfo.from,
+        contents: letterInfo.content,
+        etc: JSON.stringify({
+          color: selectedColor,
+          font: selectedFont,
+          pattern: selectedPattern,
+        }),
+      })
+
+      navigate('/sendsuccess')
+      resetStore()
+      return res.data
+    } catch (error) {
+      console.error(error)
+    }
+  }
 
   const handlePrev = () => {
     navigate('/writeletter', {
@@ -40,7 +66,9 @@ const ChoiceLetter = () => {
         <Button variant='secondary' width={82} onClick={handlePrev}>
           이전
         </Button>
-        <Button width={245}>편지 보내기</Button>
+        <Button width={245} onClick={() => handleSendLetter(tempId, letterId)}>
+          편지 보내기
+        </Button>
       </div>
     </div>
   )
