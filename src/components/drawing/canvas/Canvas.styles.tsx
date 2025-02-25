70494cb8724173e66e77232f0a88fa2be962154d
Sumin <tnalsdldkr2@naver.com>
2025-02-25 20:32:50 +0900

    feat: 색 선택(팔레트) 추가 (#11)

--- src/components/drawing/canvas/Canvas.styles.tsx
+++ src/components/drawing/canvas/Canvas.styles.tsx
@@ -1,3 +1,4 @@
+import { colors } from '#/styles/color'
 import { css } from '@emotion/react'
 
 export const CanvasStyle = css`
@@ -8,4 +9,21 @@ export const CanvasStyle = css`
   background: rgba(230, 230, 230, 0.7);
   border-radius: 8px;
   backdrop-filter: blur(2px);
+`
+
+export const PaletteWrapper = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 14px;
+`
+
+export const PaletteStyle = (bgColor: string, selectedColor: string) => css`
+  width: 24px;
+  height: 24px;
+  border-radius: 50%;
+  background-color: ${bgColor};
+  border: ${selectedColor === bgColor ? '3px solid #CCFF1B' : `1.5px solid ${colors.white}`};
+  cursor: pointer;
+  transition: border 0.2s ease-in-out;
 `
--- src/components/drawing/canvas/Canvas.tsx
+++ src/components/drawing/canvas/Canvas.tsx
@@ -1,7 +1,32 @@
-import { CanvasStyle } from './Canvas.styles'
+import { useState } from 'react'
+import { CanvasStyle, PaletteWrapper, PaletteStyle } from './Canvas.styles'
 
 const Canvas = () => {
-  return <div css={CanvasStyle}></div>
+  const paletteColors = [
+    { color: 'Black', hex: '#000000' },
+    { color: 'White', hex: '#FFFFFF' },
+    { color: 'Red', hex: '#F8596A' },
+    { color: 'Orange', hex: '#FF9061' },
+    { color: 'Yellow', hex: '#FFDE55' },
+    { color: 'Green', hex: '#39E7BA' },
+    { color: 'Blue', hex: '#388CFF' },
+    { color: 'Purple', hex: '#B072F8' },
+  ]
+  const [selectedColor, setSelectedColor] = useState<string>('#000000')
+
+  return (
+    <div css={CanvasStyle}>
+      <div css={PaletteWrapper}>
+        {paletteColors.map(({ color, hex }) => (
+          <button
+            key={color}
+            css={PaletteStyle(hex, selectedColor)}
+            onClick={() => setSelectedColor(hex)}
+          />
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Canvas
