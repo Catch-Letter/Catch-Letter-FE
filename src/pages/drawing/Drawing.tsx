2f09594a64f4611b57db42ff29742e4468c58903
Sumin <tnalsdldkr2@naver.com>
2025-02-26 21:01:19 +0900

    fix: answer 유효성 검사 수정 (#11)

--- src/pages/drawing/Drawing.tsx
+++ src/pages/drawing/Drawing.tsx
@@ -11,7 +11,23 @@ const Drawing = () => {
   const [isDrawingMode, setIsDrawingMode] = useState(false)
   const navigate = useNavigate()
   const specialCharRegex = /[^a-zA-Z0-9가-힣]/
-  const isInvalid = answer.trim().length > 8 || specialCharRegex.test(answer)
+  const isInvalid =
+    answer.trim().length === 0 || answer.trim().length > 8 || specialCharRegex.test(answer)
+
+  const getInvalidMessage = (answer: string) => {
+    if (answer.trim().length === 0) {
+      return '암호를 적어주세요! :('
+    }
+    if (answer.trim().length > 8) {
+      return '8자를 초과할 수 없어요! :('
+    }
+    if (/[^a-zA-Z0-9가-힣]/.test(answer)) {
+      return '특수문자 및 띄어쓰기는 사용할 수 없어요! :('
+    }
+    return ''
+  }
+
+  const invalidMessage = getInvalidMessage(answer)
 
   return (
     <div css={DrawingWrapper}>
@@ -26,11 +42,7 @@ const Drawing = () => {
             isInvalid={isInvalid}
             helpMessage='* 한글 , 영문 모두 8자까지 가능해요!'
             validMessage='재밌는 암호가 될 거 같아요! :)'
-            invalidMessage={
-              answer.trim().length > 8
-                ? '8자를 초과할 수 없어요! :('
-                : '특수문자는 사용할 수 없어요! :('
-            }
+            invalidMessage={invalidMessage}
             maxLength={8}
           />
         </div>
