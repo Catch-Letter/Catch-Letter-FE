f3f4b42a4f49df687fb66e320ae14d16412ca9fb
jizero <jizero27@gmail.com>
2025-03-18 21:56:45 +0900

    fix: create-post 한영 변환 적용 (#68)

--- src/components/create-post/turn-card/TurnCard.tsx
+++ src/components/create-post/turn-card/TurnCard.tsx
@@ -1,14 +1,16 @@
 import { lock, santa } from '#/assets/create'
 import { Card } from '#/shared/ui'
+import { useTranslation } from 'react-i18next'
 import { FrontCard, BackCard, TurnCardStyle } from './TurnCard.styles'
 
 const TurnCard = () => {
+  const { t } = useTranslation()
+
   return (
     <div css={TurnCardStyle}>
       <Card css={FrontCard}>
         <img src={lock} alt='lock' width={50} />
-        <div className='top'>그림을 맞추면</div>
-        <div className='bottom'>편지를 읽을 수 있어요!</div>
+        <div className='top'>{t('create.cardMessage')}</div>
       </Card>
       <Card css={BackCard}>
         <img src={santa} alt='santa' />
