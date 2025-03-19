9f62ca036c18eed58ecade73e7be51103911527a
jizero <jizero27@gmail.com>
2025-03-19 21:32:10 +0900

    fix: sendLetter margin 수정 (#68)

--- src/pages/letter-send/SendLetter.styles.tsx
+++ src/pages/letter-send/SendLetter.styles.tsx
@@ -4,23 +4,22 @@ import { css } from '@emotion/react'
 export const sendLetterWrapper = css`
   display: flex;
   flex-direction: column;
-  height: 90%;
+  height: 100%;
 
   .button-area {
     display: flex;
     flex-direction: column;
     gap: 14px;
-    margin: 0 auto;
+    margin: 0 auto 20px;
   }
 `
 export const SendLetterStyle = css`
-  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  margin-bottom: 100px;
   flex-grow: 1;
   text-align: center;
+  margin-bottom: 100px;
   color: ${colors.white};
 `
