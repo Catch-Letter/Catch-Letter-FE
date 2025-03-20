7fe395e1dcc4c7edefc73075859a651644e24768
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-03-20 11:18:58 +0900

    feat: 우체통 상태를 조회하는 훅 생성 (useInboxStatus) (#71)

--- src/api/uuid.ts
+++ src/api/uuid.ts
@@ -3,9 +3,16 @@ import { API_ENDPOINTS } from '#/api/apiEndpoints'
 
 export const fetchUUID = async (uuid: string) => {
   try {
-    const res = await apiClient.get(API_ENDPOINTS.INFO_UUID(uuid))
+    const res = await apiClient.get<Response>(API_ENDPOINTS.INFO_UUID(uuid))
     return res.data
   } catch (error) {
     console.error(error)
+    throw error
   }
+}
+
+interface Response {
+  expired_at: string
+  letter_count: number
+  name: string
 }
---
+++ src/hooks/useInboxStatus.ts
@@ -0,0 +1,40 @@
+import { fetchUUID } from '#/api/uuid'
+import { useQuery } from '@tanstack/react-query'
+import dayjs from 'dayjs'
+import utc from 'dayjs/plugin/utc'
+
+dayjs.extend(utc)
+
+export default function useInboxStatus(uuid: string) {
+  const { data, isPending, error } = useQuery({
+    queryKey: ['inbox-status', uuid],
+    queryFn: () => fetchUUID(uuid),
+  })
+
+  if (!data) {
+    return {
+      // 초기값
+      isExpired: false,
+      isPending,
+      error,
+      time_left: '23:59:59',
+      letter_count: 0,
+      name: '',
+    }
+  }
+
+  const { expired_at, letter_count, name } = data
+
+  const isExpired = dayjs().isAfter(expired_at)
+
+  const time_left = dayjs(dayjs(expired_at).diff(dayjs())).utc().format('HH:mm:ss')
+
+  return {
+    isExpired,
+    isPending,
+    error,
+    time_left,
+    letter_count,
+    name,
+  }
+}
