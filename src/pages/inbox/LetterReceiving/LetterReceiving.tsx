d883b8d0ec79e684391659f92ec21a059f8e3b4f
kimpra <dustmqaksdltkfrlfdldi@gmail.com>
2025-03-24 20:09:53 +0900

    fix: fetch uuid의 변경된 응답 적용 (#85)

--- src/pages/inbox/LetterReceiving/LetterReceiving.tsx
+++ src/pages/inbox/LetterReceiving/LetterReceiving.tsx
@@ -1,27 +1,26 @@
 import { FallingLetters, TextSection } from '#/components/inbox'
 import { useInboxStatus } from '#/hooks'
-import { Header } from '#/shared/ui'
+import { Flex, Header } from '#/shared/ui'
 import { Button } from '#/shared/ui/button'
 import { FC } from 'react'
-import { containerStyles, headerStyles } from '../Inbox.styles'
-import { bottomButtonStyles } from './LetterReceiving.styles'
+import { bottomButtonStyles, containerStyles, headerStyles } from '../Inbox.styles'
 
 interface Props {
   uuid: string
 }
 
 const LetterReciving: FC<Props> = ({ uuid }) => {
-  const { time_left, letter_count } = useInboxStatus(uuid)
+  const { name, time_left, total_letter_count } = useInboxStatus(uuid)
 
   return (
     <div css={containerStyles}>
       <Header css={headerStyles} Left={<span className='left'>catch letter</span>} />
 
       <TextSection
-        title1='우체통 마감까지'
+        title1={`${name}의 우체통 마감까지`}
         value1={time_left}
         title2='지금까지 받은 편지'
-        value2={letter_count}
+        value2={total_letter_count}
       />
 
       <Button onClick={() => {}} width={'calc(100% - 32px)'} css={bottomButtonStyles}>
