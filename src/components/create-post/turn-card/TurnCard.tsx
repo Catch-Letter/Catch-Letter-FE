1558b59ee709ce7871648e6a866d8433e6dbd9eb
jizero <jizero27@gmail.com>
2025-03-26 13:51:17 +0900

    fix: 메인 turnCard 줄바꿈 처리 및 굵기 변경 (#102)

--- src/components/create-post/turn-card/TurnCard.tsx
+++ src/components/create-post/turn-card/TurnCard.tsx
@@ -1,7 +1,7 @@
 import { lock, santa } from '#/assets/create'
 import { Card } from '#/shared/ui'
 import { useTranslation } from 'react-i18next'
-import { FrontCard, BackCard, TurnCardStyle } from './TurnCard.styles'
+import { FrontCard, BackCard, TurnCardStyle, boldFont } from './TurnCard.styles'
 
 const TurnCard = () => {
   const { t } = useTranslation()
@@ -10,7 +10,11 @@ const TurnCard = () => {
     <div css={TurnCardStyle}>
       <Card css={FrontCard}>
         <img src={lock} alt='lock' width={50} />
-        <div className='top'>{t('create.cardMessage')}</div>
+        <span className='top'>
+          <b css={boldFont}>{t('create.cardMessageTop')}</b>
+          <br />
+          {t('create.cardMessageBottom')}
+        </span>
       </Card>
       <Card css={BackCard}>
         <img src={santa} alt='santa' />
