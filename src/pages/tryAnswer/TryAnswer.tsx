06e434fae2e0412523d628b4d406fb50a25d4565
Sonseongoh <nobleser@naver.com>
2025-03-28 04:30:19 +0900

    refactor: tryAnswer 불필요한 코드 제거 및 fetch함수 tanstackquery로 처리(#110)

--- src/pages/tryAnswer/TryAnswer.tsx
+++ src/pages/tryAnswer/TryAnswer.tsx
@@ -1,10 +1,9 @@
-import { getAnswerStatus } from '#/api/getAnswerStatus'
-import { getDraw } from '#/api/getDraw'
-import { letter } from '#/api/letter'
 import { postTryAnswer } from '#/api/postTryAnswer'
 import { BackHeader } from '#/components'
-import { TryIntro } from '#/components/try-answer' // DrawingIntro import 추가
+import { TryIntro } from '#/components/try-answer'
 import { TryCounter } from '#/components/try-answer/try-Counter'
+import useGetAnswerStatus from '#/hooks/query/useGetAnswerStatus'
+import useGetDrawData from '#/hooks/query/useGetDrawData'
 import {
   LetterCardStyle,
   SkeletonCardStyle,
@@ -36,80 +35,36 @@ const TryAnswer = () => {
   const [inputValue, setInputValue] = useState<string>('')
   const [isCorrect, setIsCorrect] = useState<boolean>(false)
   const [responseMessage, setResponseMessage] = useState<string | null>(null)
-  const [letterData, setLetterData] = useState<{
-    to: string
-    from: string
-    content: string
-  } | null>(null)
+
   const [imageUrl, setImageUrl] = useState<string | null>(null)
   const [buttonText, setButtonText] = useState<string>(t('submit'))
   const [isTryStarted, setIsTryStarted] = useState<boolean>(false)
 
-  //편지내용 가져오기
-  useEffect(() => {
-    const fetchLetterData = async () => {
-      if (uuid && id) {
-        try {
-          const response = await letter(uuid, Number(id))
-          if (response && response.data) {
-            setLetterData({
-              to: response.data.to,
-              from: response.data.from,
-              content: response.data.contents,
-            })
-          }
-        } catch (error) {
-          console.error('Error fetching letter data:', error)
-        }
-      }
-    }
+  const { data: drawData } = useGetDrawData(uuid!, Number(id!))
+  const { data: answerStatusData } = useGetAnswerStatus(uuid!, Number(id!))
 
-    fetchLetterData()
-  }, [uuid, id])
-
-  //그림가져오기
+  //그림 가져오기
   useEffect(() => {
-    const getDrawData = async () => {
-      if (uuid && id) {
-        try {
-          const response = await getDraw(uuid, Number(id))
-          if (response && response.data && response.data.presigned_url) {
-            setImageUrl(response.data.presigned_url)
-          }
-        } catch (error) {
-          console.error('getDrawError:', error)
-        }
-      }
+    if (drawData && drawData.data.presigned_url) {
+      setImageUrl(drawData.data.presigned_url)
     }
-    getDrawData()
-  }, [uuid, id])
+  }, [drawData])
 
   //정답 상태 가져오기
   useEffect(() => {
-    if (uuid && id) {
-      const fetchAnswerStatus = async () => {
-        try {
-          const response = await getAnswerStatus(uuid, Number(id))
-          if (response && response.data) {
-            // response.data가 빈 배열일 경우(이미 맞춘 정답에 접근하는 경우)
-            if (Array.isArray(response.data) && response.data.length === 0) {
-              setIsCorrect(true) // 이미 맞춘 정답 표시
-              setResponseMessage(t('tryAnswer.correctAnswer'))
-              setButtonText(t('tryAnswer.checkAnswer'))
-            } else {
-              const remainingChances = extractRemainingChances(response.message)
+    if (answerStatusData) {
+      if (Array.isArray(answerStatusData) && answerStatusData.length === 0) {
+        setIsCorrect(true) // 이미 맞춘 정답 표시
+        setResponseMessage(t('tryAnswer.correctAnswer'))
+        setButtonText(t('tryAnswer.checkAnswer'))
+      } else {
+        const remainingChances = extractRemainingChances(answerStatusData.message)
 
-              setResponseMessage(t('tryAnswer.remainingAttempts', { chance: remainingChances }))
-              setChances(3 - response.data.try)
-            }
-          }
-        } catch (error) {
-          console.error('Error fetching answer status:', error)
-        }
+        setResponseMessage(t('tryAnswer.remainingAttempts', { chance: remainingChances }))
+        setChances(3 - answerStatusData.data.try)
       }
-      fetchAnswerStatus()
     }
-  }, [uuid, id])
+  }, [answerStatusData])
 
   useEffect(() => {
     if (chances === 0) {
@@ -187,7 +142,7 @@ const TryAnswer = () => {
         >
           {!isTryStarted ? (
             <TryIntro onStart={() => setIsTryStarted(true)} />
-          ) : letterData ? (
+          ) : drawData ? (
             <div css={LetterCardStyle(imageUrl || '')}></div>
           ) : (
             <div css={SkeletonCardStyle}>
