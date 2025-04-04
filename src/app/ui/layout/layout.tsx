4db08140646b0d1165829c1a74cf763dcbca7748
Sumin <tnalsdldkr2@naver.com>
2025-04-05 00:05:33 +0900

    fix: safari에서 100vh로 화면 크기를 설정할 때 발생하는 스크롤 문제 해결 (#139)

--- src/app/ui/layout/layout.tsx
+++ src/app/ui/layout/layout.tsx
@@ -1,7 +1,25 @@
-import { containerStyles } from './layout.styles'
+import { useEffect } from 'react'
 import { Outlet } from 'react-router'
+import { containerStyles } from './layout.styles'
 
 const Layout = () => {
+  useEffect(() => {
+    const setFullHeight = () => {
+      const height = window.innerHeight
+      document.documentElement.style.setProperty('--vh', `${height}px`)
+    }
+
+    window.addEventListener('resize', setFullHeight)
+    window.addEventListener('load', setFullHeight)
+
+    setFullHeight()
+
+    return () => {
+      window.removeEventListener('resize', setFullHeight)
+      window.removeEventListener('load', setFullHeight)
+    }
+  }, [])
+
   return (
     <main css={containerStyles}>
       <Outlet />
