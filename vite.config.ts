c0e822da3d28ccb652b3fc33f4a7d7f5fb8c66fb
Sumin <tnalsdldkr2@naver.com>
2025-04-08 11:02:12 +0900

    fix: preview 배포 시 라우팅 오류 해결을 위한 .env 설정 추가

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-const basePath = process.env.PR_PREVIEW_PATH || '/'
+const basePath = process.env.VITE_ROUTER_BASENAME || '/'
 
 // https://vite.dev/config/
 export default defineConfig({
