55b99251155cac2f39d28cea0dca1dc345396509
Sonseongoh <nobleser@naver.com>
2025-04-09 13:28:26 +0900

    feat: tanstackquery의 retry 속성 변경으로 특정 경우에만 1회 재시도 나머지는 3회 재시도로 변경(#171)

--- src/hooks/query/useGetAnswerStatus.ts
+++ src/hooks/query/useGetAnswerStatus.ts
@@ -5,6 +5,12 @@ const useGetAnswerStatus = (uuid: string, letterId: number) => {
   return useQuery({
     queryKey: ['getAnswerStatus', uuid, letterId],
     queryFn: () => getAnswerStatus(uuid, letterId),
+    retry: (failureCount, error) => {
+      if (error instanceof Error && error.name === 'NotFound') {
+        return failureCount < 1
+      }
+      return failureCount < 3
+    },
   })
 }
 
