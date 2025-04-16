a4890aa943dc3f384946430a5c181eeeb57037e1
Sumin <tnalsdldkr2@naver.com>
2025-04-17 01:08:28 +0900

    fix: 빌드 에러 해결 (#200)

--- vite.config.ts
+++ vite.config.ts
@@ -6,15 +6,24 @@ import path from 'path'
 
 const basePath = process.env.PR_PREVIEW_PATH || '/'
 
+const isLocal = process.env.NODE_ENV !== 'production'
+const keyPath = path.resolve(__dirname, 'cert/catchletter.kr-key.pem')
+const certPath = path.resolve(__dirname, 'cert/catchletter.kr.pem')
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   base: basePath,
-  server: {
-    host: '0.0.0.0',
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'cert/catchletter.kr-key.pem')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'cert/catchletter.kr.pem')),
-    },
-  },
+  server:
+    isLocal && fs.existsSync(keyPath) && fs.existsSync(certPath)
+      ? {
+          host: '0.0.0.0',
+          https: {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certPath),
+          },
+        }
+      : {
+          host: '0.0.0.0',
+        },
 })
