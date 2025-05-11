4d4240e09c924d3e7454c7b3707ce3c2679a0e0b
Sumin <tnalsdldkr2@naver.com>
2025-05-12 00:04:37 +0900

    feat: [/success] 페이지 [내 우체통 보러가기] 버튼 트래킹 추가 (#223)

--- src/pages/success/Success.tsx
+++ src/pages/success/Success.tsx
@@ -6,6 +6,7 @@ import { useLocation, useNavigate } from 'react-router'
 import { SuccessStyle, SuccessWrapper } from './Success.styles'
 import { ShareModal } from '#/components/share-modal'
 import useModal from '#/hooks/useModal'
+import { trackBtnClick } from '#/shared/utils/gtag'
 
 // from createPostForm
 const Success = () => {
@@ -16,6 +17,11 @@ const Success = () => {
   const { isOpen, openModal, closeModal } = useModal()
   // const { leftTime } = useCountdownTimer(user.expired)
   const uuidMatch = user.mailboxUrl.match(/[^/]+$/)
+
+  const onClickBtnPost = () => {
+    trackBtnClick('btnPostFromSuccess')
+    navigate(`/inbox/${uuidMatch?.[0]}`)
+  }
 
   return (
     <div css={SuccessStyle}>
@@ -31,12 +37,7 @@ const Success = () => {
             {t('create.check')}
           </DescWithNum> */}
         </div>
-        <Button
-          className='btn_share'
-          variant='secondary'
-          width={343}
-          onClick={() => navigate(`/inbox/${uuidMatch?.[0]}`)}
-        >
+        <Button className='btn_share' variant='secondary' width={343} onClick={onClickBtnPost}>
           {t('create.btnPost')}
         </Button>
         <Button className='btn_share' width={343} onClick={openModal}>
