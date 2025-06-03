314fc9b7094af531500641f501d7cc896813ef09
Sumin <tnalsdldkr2@naver.com>
2025-06-03 19:44:30 +0900

    refactor: 편지함 api 1분마다 자동호출되도록 변경 (#232)

--- src/api/myLetters.ts
+++ src/api/myLetters.ts
@@ -35,8 +35,7 @@ export const useMyLettersQuery = (uuid: string) => {
       }),
     initialPageParam: null,
     getNextPageParam: (lastPage) => lastPage?.next_cursor ?? null,
-    // staleTime: Infinity,
-    // refetchOnMount: false,
-    // refetchOnWindowFocus: false,
+    refetchInterval: 1 * 60 * 1000,
+    refetchOnWindowFocus: false,
   })
 }
